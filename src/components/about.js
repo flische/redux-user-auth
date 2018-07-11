974ca263fb675976e1d66ad5a18144bb40a05f41
Scott Bowler <scott.bowler@learningfuze.com>
2018-07-11 15:36:24 -0700

    Added routes and nav bar

---
+++ src/components/about.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default props => {
+    return (
+        <div>
+            <h1 className="text-center">About Us</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat ullam necessitatibus illum reiciendis eos est eum quo minima nulla sed enim, ducimus sint impedit delectus placeat, totam optio ipsam qui?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis praesentium qui tempora ipsam odit, deserunt cumque culpa ratione. Labore, a! Consectetur, dignissimos enim? Molestiae ratione ducimus eaque harum. Asperiores, eum.</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum aliquid nesciunt vero maiores dolorem enim, nostrum vel quo accusantium, iste quibusdam? Asperiores, quos? Rerum optio fuga cupiditate velit soluta distinctio!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci cum dolores culpa esse animi blanditiis ducimus rem! Quaerat dolor fugiat nesciunt, aspernatur ullam voluptates, doloremque quae, excepturi itaque iusto nobis.</p>
+        </div>
+    )
+}
