eba2575284527cab912288f040708d30d1ad6542
Scott Bowler <scott.bowler@learningfuze.com>
2018-07-13 17:45:46 -0700

    Completed user sign in and pull movie quote

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,16 +5,55 @@ const BASE_URL = 'http://api.reactprototypes.com';
 
 export function createAccount(userInfo){
     return async (dispatch) => {
-        const resp = await axios.post(`${BASE_URL}/signup`, userInfo);
+        try {
+            const resp = await axios.post(`${BASE_URL}/signup`, userInfo);
 
-        console.log('Sign Up Response:', resp);
+            localStorage.setItem('token', resp.data.token);
+
+            dispatch({ type: types.SIGN_UP });
+        } catch(err) {
+            console.log('Sign Up Error:', err.message);
+        }
     }
 }
 
-export function signIn(){
-    return { type: types.SIGN_IN };
+export function accountSignIn(userInfo){
+    return async dispatch => {
+        try {
+            const resp = await axios.post(`${BASE_URL}/signin`, userInfo);
+
+            localStorage.setItem('token', resp.data.token);
+
+            dispatch({ type: types.SIGN_IN });
+        } catch(err){
+            console.log('Error Signing in:', err.message);
+        }
+    }
 }
 
 export function signOut(){
+    localStorage.removeItem('token');
+
     return { type: types.SIGN_OUT };
+}
+
+export function getMovieQuote(){
+    return async dispatch => {
+        try {
+            const axiosConfig = {
+                headers: {
+                    authorization: localStorage.getItem('token')
+                }
+            }
+
+            const resp = await axios.get(BASE_URL, axiosConfig);
+
+            dispatch({
+                type: types.GET_MOVIE_QUOTE,
+                quote: resp.data.message
+            });
+        } catch(err){
+            console.log('Get Quote Error:', err.message);
+        }
+    }
 }
